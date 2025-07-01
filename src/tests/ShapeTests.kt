01cdfdfa3f5d0eb19bf21360b046032e59394c79
Ethan Ford <ethanfordj5@gmail.com>
2025-07-01 15:43:54 -0700

    updated unit tests

--- src/tests/ShapeTests.kt
+++ src/tests/ShapeTests.kt
@@ -66,4 +66,36 @@ class ShapeTests {
         )
     }
 
+    @Test fun slopeLine() {
+        var line = Line(Point(-6.0,0.0), Point(0.0, 2.0) )
+
+        assertEquals(
+            "The computation of the slope of the line isn't correct",
+            1.0 / 3.0,
+            line.getSlope(),
+            0.0001
+        )
+    }
+
+    @Test(expected = IllegalArgumentException::class)
+    fun invalidCircleRadius() {
+        Circle(Point(0.0, 0.0), -3.0)
+    }
+
+
+    @Test fun movePoint() {
+        val point = Point(2.0, 3.0)
+        point.move(1.0, -1.0)
+        assertEquals(3.0, point.getX(), 0.0001)
+        assertEquals(2.0, point.getY(), 0.0001)
+    }
+
+
+    @Test fun clonePoint() {
+        val original = Point(1.0, 1.0)
+        val copy = original.clone()
+        assertEquals(original.getX(), copy.getX(), 0.0001)
+        assertEquals(original.getY(), copy.getY(), 0.0001)
+        assertNotSame(original, copy)
+    }
 }
